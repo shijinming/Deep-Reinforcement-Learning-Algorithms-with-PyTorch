ccfda250163903db0b0a8c3d778b1a7e67e4de6d
shijinmings <shijinmings@outlook.com>
2019-10-13 12:43:55 +0800

    ...

--- environments/VEC_Environment.py
+++ environments/VEC_Environment.py
@@ -14,11 +14,10 @@ class VEC_Environment(gym.Env):
     environment_name = "Vehicular Edge Computing"
 
     def __init__(self, num_vehicles=500, num_tasks=100, with_V2V=True, with_V2R=True):
-        self.num_vehicles = num_vehicles
         self.max_num_of_task_generate_per_step = num_tasks
         self.vehicles_for_offload = 1
         if with_V2V:
-            self.vehicles_for_offload = self.num_vehicles
+            self.vehicles_for_offload = num_vehicles
         self.RSU_for_offload = 0
         if with_V2R:
             self.RSU_for_offload = 10
@@ -29,7 +28,7 @@ class VEC_Environment(gym.Env):
         self.mu = 0.1 #ms
         self.maxL = 10000 #m, max length of road
         self.maxV = 30
-        self.possible_states = [self.maxL]*(self.vehicles_for_offload) + [freq_level]*(self.vehicles_for_offload+self.RSU_for_offload) 
+        self.possible_states = [self.maxL]*(self.vehicles_for_offload) + [self.freq_level]*(self.vehicles_for_offload+self.RSU_for_offload) 
         self.possible_actions = [self.vehicles_for_offload+self.RSU_for_offload]*self.max_num_of_task_generate_per_step + [self.cost_level]*self.max_num_of_task_generate_per_step
         self.action_space = spaces.MultiDiscrete(self.possible_actions)
         self.observation_space = spaces.MultiDiscrete(self.possible_states)
@@ -56,90 +55,108 @@ class VEC_Environment(gym.Env):
     def reset(self):
         """Resets the environment and returns the start state"""
         self.step_count = 0
-        self.state = [self.location_to_state(self.current_user_location), self.location_to_state(self.current_goal_location)]
         self.next_state = None
         self.reward = None
         self.done = False
-        self.achieved_goal = self.state[:self.state_only_dimension]
-        self.s = np.array(self.state[:self.state_only_dimension])
+        self.s = np.array([0]*self.vehicles_for_offload+[0]*self.vehicles_for_offload+[self.RSU_F]*self.RSU_for_offload)
         return self.s
 
     def step(self, action):
         self.step_count += 1
         self.tasks = []
         self.generate_tasks(self.max_num_of_task_generate_per_step)
-        self.next_state,self.reward = self.calculate_next_state(action)
+        self.next_state,self.reward = self.step_next_state(action)
         if len(self.vehicles)==0: 
             self.done = True
         else: 
             self.done = False
-        self.state = self.next_state
-        self.s = np.array(self.next_state[:self.state_only_dimension])
         if np.random.choice(range(100)) < 10:
             self.add_vehicle()
-
-        return self.s, self.reward, self.done, {}
+        return self.next_state, self.reward, self.done, {}
 
     def step_next_state(self, action):
-        state = [0]*self.num_vehicles + [0]*self.num_vehicles
+        state = [0]*len(self.possible_states)
         self.move_vehicles()
+        self.tasks_update()
         for i in range(len(self.tasks)):
-
-            self.vehicles[action[i]]["tasks"].append([self.tasks[i]["start_time"], self.tasks[i]["max_t"], freq])
+            self.tasks[i]["cost"] = action[i+self.max_num_of_task_generate_per_step]
+            self.vehicles[action[i]]["new_tasks"].append(self.tasks[i])
+        reward = self.compute_reward()
         for v in self.vehicles:
             state[v["id"]] = v["position"]
-            f_tmp = 0
-            for j in v["tasks"]:
-
-        return np.array(state)
-        
+            state[v["id"]+self.vehicles_for_offload] = v["freq"] - sum([i["freq"] for i in v["tasks"]])
+        return np.array(state), reward
 
     def compute_reward(self):
         """Computes the reward we would have got with this achieved goal and desired goal. Must be of this exact
         interface to fit with the open AI gym specifications"""
-        for i in range(len(self.tasks)):
-
+        reward = 0
+        for v in self.vehicles:
+            freq_remain = v["freq"] - sum([i["freq"] for i in v["tasks"]])
+            if freq_remain <= 0:
+                reward += sum([-np.log(1+i["max_t"]) for i in v["new_tasks"]])
+            else:
+                own_tasks = [i for i in v["new_tasks"] if i["source"]==v["id"]]
+                other_tasks = [i for i in v["new_tasks"] if i["source"]!=v["id"]]
+                for i in own_tasks:
+                    freq = i["compute_size"]/(i["max_t"]-self.mu)
+                    if freq > freq_remain:
+                        reward += -np.log(1+i["max_t"])
+                    else:
+                        i["freq"] = freq
+                        freq_remain -= freq
+                        reward += np.log(1+self.mu)
+                        v["tasks"].append(i)
+                if freq_remain <= 0:
+                    reward += sum([-np.log(1+task["max_t"]) for task in other_tasks])
+                else:
+                    total_cost = [task["cost"] for task in other_tasks]
+                    for i in other_tasks:
+                        freq = i["cost"]/total_cost*freq_remain
+                        dp = abs(v["position"] - i["source"]["position"])
+                        snr = self.snr_ref*(dp**-2) if dp>10 else self.snr_ref
+                        delay = dp/(self.bandwidth*np.log2(1+snr)) + i["data_size"]/(freq)
+                        if delay < i["max_t"]:
+                            i["freq"] = freq
+                            reward += np.log(1+(i["max_t"]-delay))
+                            v["tasks"].append(i)
+                        else:
+                            reward += -np.log(1+i["max_t"])
+        return reward
 
     def add_vehicle(self):
-        if self.vehicle_count > self.numVehicles:
+        if self.vehicle_count > self.vehicles_for_offload:
             return
-        v_f = np.random.choice(vehicle_F)
+        v_f = np.random.choice(self.vehicle_F)
         v_p = 0
         v_v = np.random.choice(self.velocity)
-        vehicles.append({"id":self.vehicle_count, "freq":v_f, "position":v_p, "velocity":v_v, "tasks":[]})
+        self.vehicles.append({"id":self.vehicle_count, "freq":v_f, "position":v_p, "velocity":v_v, "tasks":[], "new_tasks":[]})
 
     def move_vehicles(self):
-        for i in range(len(vehicles)):
-            vehicles[i]["position"]+=vehicles[i]["velocity"]*slot/1000.0
-            if vehicles[i]["position"] > maxL:
-                vehicles.pop(i)
+        for i in range(len(self.vehicles)):
+            self.vehicles[i]["position"]+=self.vehicles[i]["velocity"]*self.slot/1000.0
+            if self.vehicles[i]["position"] >self. maxL:
+                self.vehicles.pop(i)
 
     def generate_tasks(self, task_num):
         v_id = [v["id"] for v in self.vehicles]
         v_w = [x%20+1 for x in v_id]
-        for i in range(task_num):
-            data_size = random.randint()
-            compute_size = random.randint()
-            max_delay = random.randint()
-            t_total = 0
+        for _ in range(task_num):
+            data_size = random.randint(1,10)
+            compute_size = random.randint(1,10)
+            max_t = random.randint(1,10)
             start_time = self.step_count * self.slot
             source = random.choices(v_id, weights=v_w, k=1)[0]
-            self.tasks.append({"data_size":data_size, "compute_size":compute_size, "max_t":max_t, "start_time":start_time, "source":source})
+            freq = 0
+            self.tasks.append({"data_size":data_size, "compute_size":compute_size, "max_t":max_t, "start_time":start_time, "source":source, "freq":freq, "cost":cost})
 
-    def compute_delay(self, task, action):
-        delay = 0
-        if task[3]==action["device"]:
-            delay = self.mu
-        else:
-            dp = abs(self.vehicles[task["source"]]["position"] - self.vehicles[action["device"]]["position"])
-            delay += task["data_size"]/(self.snr_ref*(dp**self.snr_alpha))
-            delay += task["compute_size"]/get_freq_allocation(vehicles[action["device"]],task)
-        return delay
-
-    def get_freq_allocation(self, vehicle, task，price):
-        for v in range(len(self.vehicles)):
+    def tasks_update(self):
+        for v in self.vehicles:
+            tmp=[]
+            for i in v["tasks"]:
+                if self.step_count*self.slot - i[0] < i[1]:
+                    tmp.append(i)
+            v["tasks"] = tmp
 
 
-    def tasks_allocation(self):
-        for v in range(len(self.vehicles)):
-            if 
+
